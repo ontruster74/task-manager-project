4a7321e003dc69fbd71296d38ed363a100115725
ontruster74 <ontruster74@gmail.com>
2025-02-15 21:04:23 -0700

    Initial commit

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::TasksController < ApplicationController
+  def index
+    render json: Task.all 
+  end
+
+  def show
+    render json: Task.find(params[:id])
+  end
+
+  def create
+    render json: Task.create(task_params)
+  end
+
+  def update
+    render json: Task.update(params[:id], task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
